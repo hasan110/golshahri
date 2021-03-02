895382e09c0b0affbcbf593c25bafd29cac5b182
morteza heydari <heydari7morteza@gmail.com>
2021-03-02 03:06:07 -0800

    new commit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -20,6 +20,7 @@ class CreateUsersTable extends Migration
             $table->string('is_block')->default(0);
             $table->string('image')->nullable();
             $table->text('auth_token');
+            $table->boolean('is_agency')->default(0);
             $table->timestamps();
         });
     }
